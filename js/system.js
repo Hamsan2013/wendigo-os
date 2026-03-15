7e7e9ba943952ee2d57169409e54b2da21edd44d
Hamsan2013 <hamsandivya@gmail.com>
2026-03-15 21:30:53 +0530

    Create system.js

---
+++ js/system.js
@@ -0,0 +1,13 @@
+function bootSystem(){
+setTimeout(function(){
+window.location.href="system/lock.html";
+},3000);
+}
+
+function unlock(){
+window.location.href="home.html";
+}
+
+function openApp(app){
+window.location.href=app;
+}
